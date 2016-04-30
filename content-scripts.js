d7673b7f8f0db3052122e5abbd72377e5e0b8c53
thedj25 <djkeenas@gmail.com>
2016-04-29 23:29:56 -0500

    Opened and started styling Created function for go to products and collections

--- content-scripts.js
+++ content-scripts.js
@@ -1,7 +1,32 @@
 //content scripts
 
 $(document).ready(function(){
-	$('body').prepend('<div id="topbar"><h1>test</h1></div>');
-	console.log('weooe');
+	var current_tab;
+	chrome.tabs.getSelected(null,function(tab) {
+		//gets current tab url
+		current_tab = tab.url;
+		console.log(current_tab);
+		//break up url 
+		var url_split = current_tab.split('/');
+		var store_url = url_split[2];
+		console.log(url_split);
+		var myobject;
+		
+		
+ 		jQuery.getJSON(current_tab + '.json', function(myobject) {
+ 			function GoToAdmin(adminpath, adminId){
+ 				console.log(adminId);
+		  		chrome.tabs.create({ url: 'http://' + store_url + adminpath + adminId });
+ 			}
+ 			var productPath = '/admin/products/';
+			var collectionPath = '/admin/collections/';
+			//decide where to go
+			if (url_split[3] === "products") {
+				//GoToAdmin(productPath, myobject.id);
+			} else if (url_split[3] === "collections") {
+				//GoToAdmin(collectionPath, myobject.collection.id);
+			} 
+		});
+	});	
 });
 
